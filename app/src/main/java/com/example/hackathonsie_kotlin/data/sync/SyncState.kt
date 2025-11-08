deff648fb7467ede405424ec3a16993c5172cd17
Robzyy <cclipicioiu@gmail.com>
2025-11-08 15:49:28 +0200

    a new beginning

--- app/src/main/java/com/example/hackathonsie_kotlin/data/sync/SyncState.kt
+++
@@ -1,8 +0,0 @@
-package com.example.hackathonsie_kotlin.data.sync
-
-sealed class SyncState {
-    object Idle : SyncState()
-    object Syncing : SyncState()
-    data class Success(val message: String = "Sync completed successfully") : SyncState()
-    data class Error(val exception: Exception) : SyncState()
-}
